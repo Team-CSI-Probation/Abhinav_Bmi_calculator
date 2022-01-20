784c1cf1b84597e23824e062548a36545cc4db3e
Avi <gautamabhinavgautam@gmail.com>
2022-01-20 19:16:23 +0530

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+const kBbottomContainerHeight = 60.0;
+const kActiveCardColor = Color(0xdc2C3266);
+const kBottomContainerColor = Color(0xff15d2eb);
+const kAppBackgroundColor = Color(0xff0a0e21);
+const kAppTextColor = Color(0xFF8d8e98);
+const kInactiveCardColor = Color(0xff4A58CF);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 60,
+  fontWeight: FontWeight.w900,
+);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+
+const kTitleText = TextStyle(
+  color: Colors.white,
+  fontSize: 22.0,
+);
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'screens/input_page.dart';
+
+void main() {
+  runApp(const BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xff4A58CF),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: InputPage(),
+    );
+  }
+}
